47259c3dd916a6043197eaa044050da0f4f38a32
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-05-01 17:22:56 +0530

    vueapp.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  base: "/vueapp/", // Replace "vueapp" with the name of your repository
+});
